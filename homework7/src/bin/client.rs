4294f39038b2e8f0d91392900bb661e3f9cca0ec
Galaxy13 <gen.hort54@gmail.com>
2023-10-01 23:23:40 +0300

    Update: changed logic of TCP connection

--- homework7/src/bin/client.rs
+++ homework7/src/bin/client.rs
@@ -1,27 +1,117 @@
 use std::net::{TcpStream};
-use std::io::{stdin, Write};
+use std::io::{Read, stdin, Write};
+use std::thread;
+use std::time::Duration;
+
+enum ClientCommand{
+    ChangeStateOn,
+    ChangeStateOff,
+    GetConsumption,
+    GetState,
+    GetName,
+    WrongCommand,
+    ExitCode
+}
+
+impl ClientCommand {
+    fn value(&self) -> &[u8]{
+        match *self {
+            ClientCommand::ChangeStateOn => &[0_u8],
+            ClientCommand::ChangeStateOff => &[1_u8],
+            ClientCommand::GetConsumption => &[2_u8],
+            ClientCommand::GetState => &[3_u8],
+            ClientCommand::GetName => &[4_u8],
+            ClientCommand::WrongCommand => &[255_u8],
+            ClientCommand::ExitCode => &[10_u8]
+        }
+    }
+}
+
+enum ExpectedAnswer{
+    Power,
+    State,
+    Name,
+    Other
+}
+
+fn handle_answer(stream: &mut TcpStream, expected_type: ExpectedAnswer) -> Result<Vec<u8>, std::io::Error>{
+    let mut buf = Vec::with_capacity(1024);
+    match stream.read_to_end(&mut buf) {
+        Ok(_) => {
+            let status_code = buf[0];
+            match status_code {
+                1u8 => println!("Request Successful"),
+                100u8 => println!("Internal Server Error"),
+                200u8 => println!("Wrong request"),
+                _ => println!("Unknown Error")
+            }
+            let msg_length = usize::from(buf[1]);
+            if msg_length != 0 {
+                let msg = &buf[2..2+msg_length];
+                match expected_type {
+                    ExpectedAnswer::Power => {
+                        let msg = [msg[0], msg[1], msg[2], msg[3]];
+                        let power = f32::from_be_bytes(msg).to_be_bytes().to_vec();
+                        Ok(power)
+                    }
+                    _ => Ok(msg.into()),
+                }
+            } else { Ok(vec![]) }
+        },
+        Err(e) => Err(e),
+    }
+}
 
 const HOST: &str = "127.0.0.1:23015";
 fn main() {
     loop {
-        let mut stream = TcpStream::connect(HOST).expect("Unable to connect to server");
         let mut command_line = String::new();
-        println!("Enter new command: \n");
+        println!("Enter new command:");
         stdin().read_line(&mut command_line).unwrap();
-        let command: Vec<&str> = command_line.split_whitespace().collect();
-        if !command.is_empty() {
-            let main_action = command[0];
-            match main_action {
-                "exit" => break,
+        let command = handle_input(command_line.trim());
+        match command {
+                ClientCommand::ExitCode => break,
+                ClientCommand::WrongCommand => println!("Wrong command!"),
                 _ => {
-                    if command.get(1).is_some(){
-                        let argument = command[1];
-                        stream.write_all(format!("{} {}", main_action, argument).as_bytes()).expect("Request failed")
+                    let mut stream = TcpStream::connect(HOST).expect("Unable to connect to server");
+                    stream.write_all(command.value()).expect("Server Unreachable");
+                    thread::sleep(Duration::from_secs_f32(0.3));
+                    match command {
+                        ClientCommand::ChangeStateOn => {
+                            handle_answer(&mut stream, ExpectedAnswer::Other).unwrap();
+                            println!("Power state changed to On");
+                        },
+                        ClientCommand::ChangeStateOff => {
+                            handle_answer(&mut stream, ExpectedAnswer::Other).unwrap();
+                            println!("Power state changed to Off");
+                        },
+                        ClientCommand::GetState => {
+                            let answer: Vec<u8> = handle_answer(&mut stream, ExpectedAnswer::State).unwrap();
+                            match answer[0] {
+                                0u8 => println!("Power State: OFF"),
+                                1u8 => println!("Power State: ON"),
+                                _ => println!("Unknown state")
+                            }
+                        }
+                        _ => {}
                     }
-                },
-            }
-        } else {
-            println!("Enter the command")
+                }
         }
+    }
+}
+
+fn handle_input(command: &str) -> ClientCommand {
+    if !command.is_empty() {
+        match command {
+            "state on" => ClientCommand::ChangeStateOn,
+            "state off" => ClientCommand::ChangeStateOff,
+            "get power" => ClientCommand::GetConsumption,
+            "get state" => ClientCommand::GetState,
+            "get name" => ClientCommand::GetName,
+            "exit" => ClientCommand::ExitCode,
+            _ => ClientCommand::WrongCommand,
+        }
+    } else {
+        ClientCommand::WrongCommand
     }
 }
--- homework7/src/bin/server.rs
+++ homework7/src/bin/server.rs
@@ -4,6 +4,32 @@ use smarthome_lib::create_powerplug;
 use smarthome_lib::control::{PowerControl, PowerState};
 use smarthome_lib::helper_traits::Info;
 
+
+enum AnswerCode {
+    Success,
+    InternalError,
+    NotFound
+}
+
+impl AnswerCode{
+    fn value(&self)  -> u8{
+        match *self {
+            AnswerCode::Success => 1u8,
+            AnswerCode::InternalError => 100u8,
+            AnswerCode::NotFound => 200u8
+        }
+    }
+}
+
+fn handle_request(code: AnswerCode, mut stream: &TcpStream, msg: &[u8]) -> Result<usize, std::io::Error> {
+    let mut buf: Vec<u8> = Vec::new();
+    let msg_size = msg.len() as u8;
+    buf.push(code.value());
+    buf.push(msg_size);
+    buf.extend_from_slice(msg);
+    stream.write(&buf)
+}
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:23015").unwrap();
     let mut power_plug = create_powerplug("Power_Plug1");
@@ -13,50 +39,63 @@ fn main() {
             continue
         }
         let stream = stream.unwrap();
-        let sender = stream.peer_addr();
+        let sender = stream.peer_addr().unwrap();
         println!("Received command from {:?}", sender);
-        let command = handle_connection(stream);
-        let res = command.trim_matches(char::from(0));
-        let command: Vec<&str> = res.split_whitespace().collect();
-        if !command.is_empty(){
-            let base_command = command[0];
-            match base_command{
-                "state" => if command.get(1).is_some(){
-                    let argument = &command[1];
-                    match argument as &str {
-                        "on" => {power_plug.power_change(PowerState::On);
-                        println!("Power is On")},
-                        "off" => {power_plug.power_change(PowerState::Off);
-                        println!("Power if Off")},
-                        _ => println!("Wrong argument!")
-                    }
-                },
-                "get" => if command.get(1).is_some(){
-                    let argument = &command[1];
-                    match argument as &str {
-                        "state" => println!("Current state: {}", power_plug.get_state()),
-                        "consumption" => println!("Current power consumption: {}", power_plug.current_consumption()),
-                        "name" => println!("Plug name: {}", power_plug.get_name()),
-                        _ => println!("Wrong argument")
-                    }
-                },
-                _ => println!("Wrong command")
+        let command = handle_connection(&stream);
+        match command {
+            [0_u8] => {
+                power_plug.power_change(PowerState::On);
+                if handle_request(AnswerCode::Success, &stream, &[],).is_err(){
+                    continue
+                }
+                println!("State of plug {} is changed to On", power_plug.get_name())
+            },
+            [1_u8] => {
+                power_plug.power_change(PowerState::Off);
+                if handle_request(AnswerCode::Success, &stream, &[],).is_err(){
+                    continue
+                }
+                println!("State of plug {} is changed to Off", power_plug.get_name())
+            },
+            [2_u8] => {
+                let power = power_plug.current_consumption().to_be_bytes();
+                if handle_request(AnswerCode::Success, &stream, &power,).is_err(){
+                    continue
+                }
+                println!("Consumption of power plug {} is {}", power_plug.get_name(), power_plug.current_consumption());
+            },
+            [3_u8] => {
+                let state_msg = match power_plug.current_state(){
+                    "Off" => [0u8],
+                    "On" => [1u8],
+                    _ => [2u8]
+                };
+                if handle_request(AnswerCode::Success, &stream, &state_msg,).is_err(){
+                    continue
+                }
+                println!("Current state of power plug {} is {}", power_plug.get_name(), power_plug.current_state());
+            },
+            [4_u8] => {
+                let plug_name = power_plug.get_name();
+                if handle_request(AnswerCode::Success, &stream, plug_name.as_bytes(),).is_err(){
+                    continue
+                }
+                println!("Name of power plug is {}", power_plug.get_name())
             }
+            _ => {}
         }
         println!("Command from {:?} completed", sender)
     }
 }
 
-fn handle_connection(mut tcp_stream: TcpStream) -> String {
-    let mut data = [0_u8; 20];
-    match tcp_stream.read(&mut data) {
-        Ok(size) => {
-            tcp_stream.write_all(&data[0..size]).unwrap();
-        },
+fn handle_connection(mut tcp_stream: &TcpStream) -> [u8; 1] {
+    let mut data = [0_u8; 1];
+    match tcp_stream.read_exact(&mut data) {
+        Ok(()) => {},
         Err(e) => {
             println!("Error {} occurred", e);
             tcp_stream.shutdown(Shutdown::Both).unwrap();
         },
     }
-    String::from_utf8_lossy(&data).to_string()
+    data
 }
